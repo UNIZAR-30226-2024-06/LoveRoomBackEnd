e7b91f0af2c966135cfbc3ea9629cb5e5c222145
AlexBenedi <843826@unizar.es>
2024-04-03 17:14:36 +0200

    Añadido getEmail v2

--- src/routes.ts
+++ src/routes.ts
@@ -110,17 +110,6 @@ router.get('/users', async (req, res) => {
 // Obtener un usuario
 router.get('/user/:correo', autenticacionController.checkAuthUser, UsuarioController.getUser);
 
-// Obtener atributos suelto de un usuario
-router.get('/user/:correo/email', autenticacionController.checkAuthUser, UsuarioController.mailAlreadyUse, UsuarioController.updateEmail);
-router.get('/user/:correo/password', autenticacionController.checkAuthUser, UsuarioController.updatePassword);
-router.get('/user/:correo/name', autenticacionController.checkAuthUser,  UsuarioController.updateName);
-router.get('/user/:correo/age', autenticacionController.checkAuthUser, UsuarioController.updateAge);
-router.get('/user/:correo/sex', autenticacionController.checkAuthUser, UsuarioController.updateSex);
-router.get('/user/:correo/description', autenticacionController.checkAuthUser, UsuarioController.updateDescription);
-router.get('/user/:correo/photo', autenticacionController.checkAuthUser, UsuarioController.updatePhoto);
-router.get('/user/:correo/location', autenticacionController.checkAuthUser, UsuarioController.updateLocation);
-router.get('/user/:correo/preferences', autenticacionController.checkAuthUser, UsuarioController.updatePreferences);
-
 // Actualizacion de un usario entero
 router.put('/user/update', autenticacionController.checkAuthUser, UsuarioController.mailAlreadyUse, UsuarioController.updateUser);
 
