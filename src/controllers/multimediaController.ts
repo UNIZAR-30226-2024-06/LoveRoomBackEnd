680340ab6157f382a8dca0b39f11579973b4d308
marasanz-unizar <839385@unizar.es>
2024-05-01 23:50:34 +0200

    Update multimediaController.ts

--- src/controllers/multimediaController.ts
+++ src/controllers/multimediaController.ts
@@ -1,48 +1,43 @@
-import { Request, Response } from "express"
-import { uploadsDirectory } from "../storage"
-import { multimediaTypes } from "../constants/multimediaTypes"
-import { createMultimedia, deleteMultimedia} from "../db/multimedia"
-
-
-
+import { Request, Response } from "express";
+import { uploadsDirectory } from "../storage";
+import { multimediaTypes } from "../constants/multimediaTypes";
+import { createMultimedia, deleteMultimedia } from "../db/multimedia";
 
 const MultimediaController = {
-
-
-    uploadFoto: async (req: Request, res: Response): Promise<any> => {
-        if (!req.file) {
-            return res.status(400).json({ error: 'No file provided' });
-        }
-        const fileName = req.file.filename;
-        const ruta = uploadsDirectory + fileName;
-        const nombreArchivo = await createMultimedia(ruta, multimediaTypes.IMAGE);
-        console.log(ruta);
-        return res.json({ nombreArchivo: fileName });
-    },
-
-    uploadVideo: async (req: Request, res: Response): Promise<any> => {
-        if (!req.file) {
-            return res.status(400).json({ error: 'No file provided' });
-        }
-        const fileName = req.file.filename;
-        const ruta = uploadsDirectory + fileName;
-        const nombreArchivo = await createMultimedia(ruta, multimediaTypes.VIDEO);
-        console.log(ruta);
-        return res.json({ nombreArchivo: fileName });
-    },
-
-    deleteMultimedia: async (req: Request, res: Response): Promise<any> => {
-        const { nombreArchivo } = req.params;
-        const rutaMultimedia = uploadsDirectory + '/' + nombreArchivo;
-        const result = await deleteMultimedia(rutaMultimedia);
-    },
-
-    getMultimedia: async (req: Request, res: Response): Promise<any> => {
-        const { nombreArchivo } = req.params;
-        const rutaMultimedia = uploadsDirectory + '/' + nombreArchivo;
-        return res.sendFile(rutaMultimedia);
+  uploadFoto: async (req: Request, res: Response): Promise<any> => {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file provided" });
     }
+    const fileName = req.file.filename;
+    const ruta = uploadsDirectory + fileName;
+    const nombreArchivo = await createMultimedia(ruta, multimediaTypes.IMAGE);
+    console.log("Ruta imagen subida " + ruta);
+    console.log("Nombre archivo " + nombreArchivo);
+    return res.json({ nombreArchivo: fileName });
+  },
 
-}
+  uploadVideo: async (req: Request, res: Response): Promise<any> => {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file provided" });
+    }
+    const fileName = req.file.filename;
+    const ruta = uploadsDirectory + fileName;
+    const nombreArchivo = await createMultimedia(ruta, multimediaTypes.VIDEO);
+    console.log(ruta);
+    return res.json({ nombreArchivo: fileName });
+  },
+
+  deleteMultimedia: async (req: Request, res: Response): Promise<any> => {
+    const { nombreArchivo } = req.params;
+    const rutaMultimedia = uploadsDirectory + "/" + nombreArchivo;
+    const result = await deleteMultimedia(rutaMultimedia);
+  },
+
+  getMultimedia: async (req: Request, res: Response): Promise<any> => {
+    const { nombreArchivo } = req.params;
+    const rutaMultimedia = uploadsDirectory + "/" + nombreArchivo;
+    return res.sendFile(rutaMultimedia);
+  },
+};
 
 export default MultimediaController;
