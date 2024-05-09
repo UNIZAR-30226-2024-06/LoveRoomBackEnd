a2362426da4b16dc5b8376e9a4d20af2c7177377
carlosssdv <145296851+carlosssdv@users.noreply.github.com>
2024-05-09 21:55:42 +0200

    Update multimediaController.ts

--- src/controllers/multimediaController.ts
+++ src/controllers/multimediaController.ts
@@ -9,9 +9,8 @@ const MultimediaController = {
       return res.status(400).json({ error: "No file provided" });
     }
     const fileName = req.file.filename;
-    const ruta = uploadsDirectory + fileName;
-    const nombreArchivo = await createMultimedia(ruta, multimediaTypes.IMAGE);
-    console.log("Ruta foto " + ruta);
+    const nombreArchivo = await createMultimedia(fileName, multimediaTypes.IMAGE);
+    console.log("Ruta foto " + uploadsDirectory + "/" + fileName);
     return res.json({ nombreArchivo: fileName });
   },
 
@@ -20,16 +19,14 @@ const MultimediaController = {
       return res.status(400).json({ error: "No file provided" });
     }
     const fileName = req.file.filename;
-    const ruta = uploadsDirectory + fileName;
-    const nombreArchivo = await createMultimedia(ruta, multimediaTypes.VIDEO);
+    const nombreArchivo = await createMultimedia(fileName, multimediaTypes.VIDEO);
     console.log("Ruta video " + ruta);
     return res.json({ nombreArchivo: fileName });
   },
 
   deleteMultimedia: async (req: Request, res: Response): Promise<any> => {
     const { nombreArchivo } = req.params;
-    const rutaMultimedia = uploadsDirectory + "/" + nombreArchivo;
-    const result = await deleteMultimedia(rutaMultimedia);
+    const result = await deleteMultimedia(nombreArchivo);
     console.log("Ruta multimedia eliminado: " + rutaMultimedia);
   },
 
