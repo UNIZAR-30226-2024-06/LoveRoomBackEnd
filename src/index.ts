e89559e675213e8770a654fe3d7e8ec53895b22e
marasanz-unizar <839385@unizar.es>
2024-05-11 14:32:33 +0200

    Content-Length en CORS

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,52 @@
 import express, { Express, Request, Response } from "express";
-import multer from 'multer';
-import path from 'path';
-import { PrismaClient } from '@prisma/client';
-import routes from './routes';
-import SocketManager from './services/socketManager';
-import { createServer } from "http";  
+import multer from "multer";
+import path from "path";
+import { PrismaClient } from "@prisma/client";
+import routes from "./routes";
+import SocketManager from "./services/socketManager";
+import { createServer } from "http";
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app: Express = express();
 const prisma = new PrismaClient();
-const port =  process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // Para parsear el body de las peticiones a JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Middleware para los headers
 app.use((req: Request, res: Response, next) => {
   // Headers de seguridad
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader("X-XSS-Protection", "1; mode=block");
 
   // Headers CORS
   // Cambiar el * por el dominio del cliente para restringir el acceso a solo ese dominio
   // Cuidado, pueden seguir accediendo desde herramientas como Postman
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Content-Length, Accept, Authorization"
+  );
 
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  if (req.method === "OPTIONS") {
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH"
+    );
     return res.status(200).json({});
   }
   // Pasamos al siguiente middleware
   next();
 });
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.use((req, res) => {
   // Not found error
-  res.status(404).json({ message: '404 Not Found' });
+  res.status(404).json({ message: "404 Not Found" });
 });
 
 const httpServer = createServer(app);
@@ -51,10 +56,9 @@ httpServer.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await prisma.$disconnect();
   process.exit();
 });
 
-
-export { app, prisma, jwt};
+export { app, prisma, jwt };
