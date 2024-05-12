d7295eab3c9e95ddd945fd686e0bd1c8195ad58d
marasanz-unizar <839385@unizar.es>
2024-05-12 14:05:50 +0200

    Limite de fichero a 50 Mb

--- src/storage.ts
+++ src/storage.ts
@@ -1,8 +1,7 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
 
-const uploadsDirectory = path.join(__dirname, 'uploads');
-
+const uploadsDirectory = path.join(__dirname, "uploads");
 
 // Configuración de Multer para almacenar los archivos en una carpeta llamada 'uploads'
 const storage = multer.diskStorage({
@@ -13,21 +12,36 @@ const storage = multer.diskStorage({
     const userId = req.params.idUsuario;
     const timestamp = Date.now();
     const randomString = Math.random().toString(36).substring(7); // Genera una cadena aleatoria
-    cb(null, userId + '-' + timestamp + '-' + randomString);
-  }
+    cb(null, userId + "-" + timestamp + "-" + randomString);
+  },
 });
 
+const limits = {
+  fileSize: 1024 * 1024 * 50, // limit to 50MB
+};
+
 // Filtrar los archivos para aceptar solo imágenes y videos
-const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
-      cb(null, true); // Aceptar el archivo
-    } else {
-      console.log('File type not supported:', file.mimetype);
-      cb(null, false); // Rechazar el archivo
-    }
-  };
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  if (
+    file.mimetype.startsWith("image/") ||
+    file.mimetype.startsWith("video/")
+  ) {
+    cb(null, true); // Aceptar el archivo
+  } else {
+    console.log("File type not supported:", file.mimetype);
+    cb(null, false); // Rechazar el archivo
+  }
+};
 
 // Configura Multer con las opciones de almacenamiento
-const upload = multer({ storage: storage, fileFilter: fileFilter});
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: limits,
+});
 
 export { uploadsDirectory, upload };
