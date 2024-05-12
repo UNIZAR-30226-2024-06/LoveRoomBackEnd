fb44d0a6daef94e72a211420ae1d0b38c036b83b
marasanz-unizar <839385@unizar.es>
2024-05-12 14:35:39 +0200

    Limite 100Mb middleware análisis del cuerpo

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 
 // Para parsear el body de las peticiones a JSON
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // Middleware para los headers
 app.use((req: Request, res: Response, next) => {
